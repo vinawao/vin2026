d445c440035e8e7df3203b386f53d97806094456
vinawao <69920630+vinawao@users.noreply.github.com>
2026-03-24 10:59:29 +0800

    Update INFO.js

--- INFO.js
+++ INFO.js
@@ -1,1 +1,9 @@
-document.write('    <strong style="color: #0ff">GEMINI TV INFO HIGHLIGHT :</strong>\n <p  style="color: lime">\nStay tuned for new features. \n<br/> \n</p>'); 
+document.write('    
+<strong style="color: #0ff">GEMINI TV INFO HIGHLIGHT :</strong>\n 
+<p  style="color: lime">
+Stay tuned for new features.
+<br/>
+Gemini Pro Android TV.
+ </p>
+
+'); 
