173b89b71dc294b9a8bb74c50ce2ee6755d094b7
vinawao <69920630+vinawao@users.noreply.github.com>
2026-03-28 15:44:30 +0800

    Update HTML_HEADER.js

--- HTML_HEADER.js
+++ HTML_HEADER.js
@@ -1,1 +1,23 @@
-document.write('\n<div class="header">\n<table border="0" style="width: 95%;">\n  <tr>\n    <td style="width: 15%; font-size: 17px; font-weight: bold; color: white"><strong style="color: red; font-size: 22px; display: inline-block;">GEMINI PRO</strong></td> \n    <td style="width: 11%; text-align: center;"><strong>Android TV</strong></td>\n    <td style="width: 44%; text-align: center;"><div id="jam" style="font-size: 14px; align: left; display: inline-block;"></div><script src="https://vinawao.github.io/vin2026/JAM.js"></script></td>\n    <td style="width: 11%;"><button class="right" data-modal="modal-info">INFO IPTV</button></td>\n    <td style="width: 9%;"><button class="right" data-modal="modal-about">ABOUT</button></td>\n  </tr>\n</table>\n</div>\n');
+document.write('\n<div class="header">\n<table border="0" style="width: 95%;">\n  <tr>\n    <td style="width: 17%; font-size: 17px; font-weight: bold; color: white"><img src="http://iptv2023.xtgem.com/2026/GEMINI/LANDSKAP/IMG/Geminipro_Androidtv.png" style="width: 190px;"></td> \n    <td style="width: 5%; text-align: center;"> </td>\n    <td style="width: 44%; text-align: center;"><div id="jam" style="font-size: 14px; align: left; display: inline-block;"></div><script src="http://iptv2023.xtgem.com/2026/GEMINI/jam.js"></script></td>\n    <td style="width: 11%;"><button class="right" data-modal="modal-info">INFO IPTV</button></td>\n    <td style="width: 9%;"><button class="right" data-modal="modal-about">ABOUT</button></td>\n  </tr>\n</table>\n</div>\n'); 
+
+
+
+//* ==== CEGAH ZOOM HALAMAN WEB 
+
+// Mencegah pinch-to-zoom (dua jari)
+document.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+// Mencegah zoom saat double tap
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
+
